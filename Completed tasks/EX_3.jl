fa9a557d8fc569950b4a24b4440acf064a0f5400
Fimaruyn <vovabelov0806@gmail.com>
2024-10-03 20:41:16 +0300

    first commit

--- Completed tasks/EX_3.jl
+++ Completed tasks/EX_3.jl
@@ -7,21 +7,42 @@
 =#
 using HorizonSideRobots
 
+# Главная функция
 function field!(robot)
-    do_upora!(robot, Sud)
-    do_upora!(robot, West)
+
+    # Запоминание исходной точки
+    s = 0; w = 0
+    s += do_upora(robot, Sud)
+    w += do_upora(robot, West)
+
+
     side = Ost; n = num_steps(robot)
+
     for i in 1:n+1
         markers!(robot, side)
         putmarker!(robot)
         side = inverse(side)
-        if isborder(robot, Nord)
+        if !isborder(robot, Nord)
+            move!(robot, Nord)
+        end
+    end
+
+    # Возвращение в исходное положение
+    do_upora!(robot, Sud)
+    do_upora!(robot, West)
+    for i in 1:s
         move!(robot, Nord)
     end
+    for k in 1:w
+        move!(robot, Ost)
+    end
+
 end
 
+# Инверсия стороны света
 inverse(side::HorizonSide) = HorizonSide((Int(side)+2)%4)
 
+# Заполнить линию
 function markers!(robot, side)
     while !isborder(robot, side)
         putmarker!(robot)
@@ -29,12 +50,24 @@ function markers!(robot, side)
     end
 end
 
+# Перемещение в заданном направлении до стенки с подсчетом клеток
+function do_upora(robot, side)
+    num_steps = 0
+    while !isborder(robot, side)
+        num_steps += 1
+        move!(robot, side)
+    end
+    return num_steps
+end
+
+# Перемещение в заданном направлении до стенки
 function do_upora!(robot, side)
     while !isborder(robot, side)
         move!(robot, side)
     end
 end
 
+# Счетчик высоты поля
 function num_steps(robot)
     n = 0
     while !isborder(robot, Nord)
