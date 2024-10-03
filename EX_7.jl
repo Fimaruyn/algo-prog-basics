f93076e61cc56f1c55d433f6efeb68fba0871347
Fimaruyn <vovabelov0806@gmail.com>
2024-10-03 21:57:23 +0300

    da

---
+++ EX_7.jl
@@ -0,0 +1,14 @@
+#=
+ДАНО: Робот - рядом с горизонтальной бесконечно продолжающейся в
+обе стороны перегородкой (под ней), в которой имеется проход шириной в одну
+клетку.
+
+РЕЗУЛЬТАТ: Робот - в клетке под проходом
+
+using HorizonSideRobots; robot = Robot("field_ex_7.sit", animate = true); include("EX_7.jl"); prohod!(robot)
+=#
+using HorizonSideRobots
+
+function prohod!(robot)
+    
+end
